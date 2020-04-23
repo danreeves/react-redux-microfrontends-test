64c98f8c3e4ded5201bffece45617e0292d7eb8e
Dan Reeves <hey@danreev.es>
2020-04-24 00:08:35 +0100

    prettier app-shell, make it private: no module published

--- packages/app-shell/src/app-shell.js
+++ packages/app-shell/src/app-shell.js
@@ -1,17 +1,15 @@
-import React, { lazy, Suspense } from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux-dynamic-modules'
-import { getSagaExtension } from 'redux-dynamic-modules-saga'
-import { Provider, useDispatch } from 'react-redux'
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux-dynamic-modules";
+import { getSagaExtension } from "redux-dynamic-modules-saga";
+import { Provider, useDispatch } from "react-redux";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { ModuleRegisterProvider } from "@danreeves/app-module-registery";
+import { Toasts, addToast } from "@danreeves/app-toasts";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom'
-import { ModuleRegisterProvider } from '@danreeves/app-module-registery'
-import { Toasts, addToast } from '@danreeves/app-toasts'
-import {NotificationHistory, module as notificationHistoryModule} from '@danreeves/app-notification-history'
+  NotificationHistory,
+  module as notificationHistoryModule,
+} from "@danreeves/app-notification-history";
 
 const store = createStore(
   {
@@ -19,45 +17,48 @@ const store = createStore(
     extensions: [getSagaExtension({})],
   },
   notificationHistoryModule
-)
+);
 
-store.subscribe(() => console.log('[Store]', store.getState()))
+store.subscribe(() => console.log("[Store]", store.getState()));
 
-const Posts = lazy(() => import('@danreeves/app-posts'))
+const Posts = lazy(() => import("@danreeves/app-posts"));
 
-function App () {
-  return <Provider store={store}>
-    <ModuleRegisterProvider>
-      <Router>
-        <div>
-          <header>
-            <h1>Hello world!</h1>
-            <nav>
-              <Link to="/">Home</Link>
-              <Link to="/posts">Posts</Link>
-              <Link to="/notifications">Notifications</Link>
-            </nav>
-          </header>
-          <section>
-            <Suspense fallback={<div>Loading...</div>}>
-              <Route exact path="/">
-                <p>This is the home page</p>
-                <button onClick={() => store.dispatch(addToast("wow"))}>Add a toast</button>
-              </Route>
-              <Route path="/posts" component={Posts} />
-              <Route path="/notifications">
-                <NotificationHistory />
-              </Route>
-            </Suspense>
-          </section>
-        </div>
-        <Toasts />
-      </Router>
-    </ModuleRegisterProvider>
-  </Provider>
+function App() {
+  return (
+    <Provider store={store}>
+      <ModuleRegisterProvider>
+        <Router>
+          <div>
+            <header>
+              <h1>Hello world!</h1>
+              <nav>
+                <Link to="/">Home</Link>
+                <Link to="/posts">Posts</Link>
+                <Link to="/notifications">Notifications</Link>
+              </nav>
+            </header>
+            <section>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Route exact path="/">
+                  <p>This is the home page</p>
+                  <button onClick={() => store.dispatch(addToast("wow"))}>
+                    Add a toast
+                  </button>
+                </Route>
+                <Route path="/posts" component={Posts} />
+                <Route path="/notifications">
+                  <NotificationHistory />
+                </Route>
+              </Suspense>
+            </section>
+          </div>
+          <Toasts />
+        </Router>
+      </ModuleRegisterProvider>
+    </Provider>
+  );
 }
 
-
-const root = document.createElement('div');
+const root = document.createElement("div");
 document.body.appendChild(root);
-ReactDOM.render(<App/>, root)
+ReactDOM.render(<App />, root);
